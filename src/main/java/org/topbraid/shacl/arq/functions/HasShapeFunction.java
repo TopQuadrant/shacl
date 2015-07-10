86fdea2a14d26054135e042ae5841061e31d9a6e
Holger Knublauch <holger@topquadrant.com>
2015-07-10 14:31:37 +1000

    Latest version with improved handling of recursion

--- src/main/java/org/topbraid/shacl/arq/functions/HasShapeFunction.java
+++ src/main/java/org/topbraid/shacl/arq/functions/HasShapeFunction.java
@@ -3,7 +3,8 @@ package org.topbraid.shacl.arq.functions;
 import org.topbraid.shacl.constraints.FatalErrorLog;
 import org.topbraid.shacl.constraints.ResourceConstraintValidator;
 import org.topbraid.shacl.vocabulary.SH;
-import org.topbraid.spin.arq.AbstractFunction3;
+import org.topbraid.spin.arq.AbstractFunction4;
+import org.topbraid.spin.util.JenaDatatypes;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.query.Dataset;
@@ -21,32 +22,50 @@ import com.hp.hpl.jena.vocabulary.RDF;
  * 
  * @author Holger Knublauch
  */
-public class HasShapeFunction extends AbstractFunction3 {
+public class HasShapeFunction extends AbstractFunction4 {
+	
+	private static ThreadLocal<Boolean> recursionIsErrorFlag = new ThreadLocal<Boolean>();
 
+	
 	@Override
-	protected NodeValue exec(Node resourceNode, Node shapeNode, Node shapesGraphNode, FunctionEnv env) {
-		if(SHACLRecursionGuard.start(resourceNode, shapeNode)) {
-			FatalErrorLog.get().log("Unsupported recursion at " + resourceNode + " against " + shapeNode);
-			throw new ExprEvalException("Unsupported recursion");
+	protected NodeValue exec(Node resourceNode, Node shapeNode, Node shapesGraphNode, Node recursionIsError, FunctionEnv env) {
+		Boolean oldFlag = recursionIsErrorFlag.get();
+		if(JenaDatatypes.TRUE.asNode().equals(recursionIsError)) {
+			recursionIsErrorFlag.set(true);
 		}
-		else {
-			
-			try {
-				Model model = ModelFactory.createModelForGraph(env.getActiveGraph());
-				Resource resource = (Resource) model.asRDFNode(resourceNode);
-				Resource shapesGraph = (Resource) model.asRDFNode(shapesGraphNode);
-				Dataset dataset = DatasetImpl.wrap(env.getDataset());
-				Resource shape = (Resource) dataset.getDefaultModel().asRDFNode(shapeNode);
-				Model results = ResourceConstraintValidator.get().validateNodeAgainstShape(
-						dataset, shapesGraph, resource.asNode(), shape.asNode(), SH.Error, null);
-				if(results.contains(null, RDF.type, SH.FatalError)) {
-					throw new ExprEvalException("Propagating fatal error from nested shapes");
+		try {
+			if(SHACLRecursionGuard.start(resourceNode, shapeNode)) {
+				if(JenaDatatypes.TRUE.asNode().equals(recursionIsError) || (oldFlag != null && oldFlag)) {
+					FatalErrorLog.get().log("Unsupported recursion at " + resourceNode + " against " + shapeNode);
+					throw new ExprEvalException("Unsupported recursion");
 				}
-				return NodeValue.makeBoolean(results.isEmpty());
+				else {
+					SHACLRecursionGuard.end(resourceNode, shapeNode);
+					return NodeValue.TRUE;
+				}
 			}
-			finally {
-				SHACLRecursionGuard.end(resourceNode, shapeNode);
+			else {
+				
+				try {
+					Model model = ModelFactory.createModelForGraph(env.getActiveGraph());
+					Resource resource = (Resource) model.asRDFNode(resourceNode);
+					Resource shapesGraph = (Resource) model.asRDFNode(shapesGraphNode);
+					Dataset dataset = DatasetImpl.wrap(env.getDataset());
+					Resource shape = (Resource) dataset.getDefaultModel().asRDFNode(shapeNode);
+					Model results = ResourceConstraintValidator.get().validateNodeAgainstShape(
+							dataset, shapesGraph, resource.asNode(), shape.asNode(), SH.Error, null);
+					if(results.contains(null, RDF.type, SH.FatalError)) {
+						throw new ExprEvalException("Propagating fatal error from nested shapes");
+					}
+					return NodeValue.makeBoolean(results.isEmpty());
+				}
+				finally {
+					SHACLRecursionGuard.end(resourceNode, shapeNode);
+				}
 			}
+		}
+		finally {
+			recursionIsErrorFlag.set(oldFlag);
 		}
 	}
 }
