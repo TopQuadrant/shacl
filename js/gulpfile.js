14e096e076e6f7145d3388205fbb5e2bdcfbd38b
Antonio Garrote <antoniogarrote@gmail.com>
2017-07-06 10:57:50 +0100

    Adding packaging information and web tests

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,3 +1,28 @@
-/**
- * Created by antoniogarrote on 23/06/2017.
- */
+var gulp = require('gulp');
+var nodeunit = require('gulp-nodeunit');
+var browserify = require('gulp-browserify');
+var fs = require('fs');
+
+gulp.task('test', function () {
+    gulp.src('./test/**/*.js')
+        .pipe(nodeunit({}));
+});
+
+gulp.task('browserify', function () {
+    if (fs.existsSync('public/index.js')) {
+        fs.unlinkSync('public/index.js');
+    }
+    if (fs.existsSync('public/shacl.js')) {
+        fs.unlinkSync('public/shacl.js');
+    }
+    gulp.src('index.js')
+        .pipe(browserify({
+            standalone: 'SHACLValidator'
+        }))
+        .pipe(gulp.dest('public'))
+        .on('end', function () {
+            fs.renameSync('public/index.js', 'public/shacl.js');
+        });
+});
+
+gulp.task('default', ['test', 'browserify']);
--- src/main/java/org/raml/amf/shapes/Validator.java
+++
@@ -1,7 +0,0 @@
-package org.raml.amf.shapes;
-
-/**
- * Created by antoniogarrote on 09/05/2017.
- */
-public class Validator {
-}
