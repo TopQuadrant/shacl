b637b0c7888c065ccce144f1e02af5ebaac2c0b2
Holger Knublauch <holger@topquadrant.com>
2020-02-17 09:51:31 +1000

    Restored accidentally overwritten changes to SHACLCWriter (incomplete anyway)

--- src/main/java/org/topbraid/shacl/compact/SHACLCWriter.java
+++ src/main/java/org/topbraid/shacl/compact/SHACLCWriter.java
@@ -76,8 +76,10 @@ public class SHACLCWriter extends WriterGraphRIOTBase {
 	
 	private void write(IndentedWriter out, Graph graph, PrefixMap prefixMap, String baseURI, Context context) {
 		Model model = ModelFactory.createModelForGraph(graph);
-		out.println("BASE <" + baseURI + ">");
-		out.println();
+		if(baseURI != null) {
+			out.println("BASE <" + baseURI + ">");
+			out.println();
+		}
 		writeImports(out, model.getResource(baseURI));
 		writePrefixes(out, prefixMap);
 		writeShapes(out, model);
@@ -137,7 +139,7 @@ public class SHACLCWriter extends WriterGraphRIOTBase {
 	
 	
 	private void writeShape(IndentedWriter out, Resource shape) {
-		out.print("" + iri(shape));
+		out.print("shape " + iri(shape));
 		writeShapeBody(out, shape);
 	}
 	
