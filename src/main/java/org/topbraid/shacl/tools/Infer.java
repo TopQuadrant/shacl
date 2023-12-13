d8b4be6b124438590773f284ae529dfd3a1a1aaf
Gabe Fierro <gtfierro@mines.edu>
2023-12-12 17:42:44 -0700

    only add to data model when necessary, try to fix formatting

--- src/main/java/org/topbraid/shacl/tools/Infer.java
+++ src/main/java/org/topbraid/shacl/tools/Infer.java
@@ -51,43 +51,48 @@ public class Infer extends AbstractTool {
 		if(shapesModel == null) {
 			shapesModel = dataModel;
 		}
-        int maxIterations = getMaxIterations(args);
+		int maxIterations = getMaxIterations(args);
 
-        // execute the rules over and over until there are no new results
-        // or until a maximum number of iterations has been reached.
-        // Keep track of all results from each iteration in a separate model.
-        // Break if no new results are found, or if the size of the allResults
-        // model does not increase after a run.
+		// execute the rules over and over until there are no new results
+		// or until a maximum number of iterations has been reached.
+		// Keep track of all results from each iteration in a separate model.
+		// Break if no new results are found, or if the size of the allResults
+		// model does not increase after a run.
 
-        // stores which iteration we are on
-        int iteration = 0;
-        // stores all results from each iteration
-        Model results = null;
-        do {
-            // execute the rules
-            Model newResults = RuleUtil.executeRules(dataModel, shapesModel, null, null);
-            // if this is the first iteration, set the results model, otherwise add to it
-            if (results == null) {
-                results = newResults;
-            } else {
-                results.add(newResults);
-            }
-            // if no new results were found, break
-            if (newResults.size() == 0) {
-                break;
-            }
-            // if the size of the allResults model did not increase, break
-            // (this means that the new results were already in the allResults model)
-            if (results.size() == newResults.size()) {
-                break;
-            }
-            // otherwise, continue by adding the new results to the data model, incrementing the iteration,
-            // and continuing the loop to see if more inferences are performed
-            dataModel.add(newResults);
-            iteration++;
-        } while (iteration < maxIterations);
+		// stores which iteration we are on
+		int iteration = 0;
+		// stores all results from each iteration
+		Model results = null;
+		do {
+			// execute the rules
+			Model newResults = RuleUtil.executeRules(dataModel, shapesModel, null, null);
+			// if this is the first iteration, set the results model, otherwise add to it
+			if (results == null) {
+				results = newResults;
+			} else {
+				results.add(newResults);
+			}
+			// if no new results were found, break
+			if (newResults.size() == 0) {
+				break;
+			}
+			// if the size of the allResults model did not increase, break
+			// (this means that the new results were already in the allResults model)
+			if (results.size() == newResults.size()) {
+				break;
+			}
 
-        // print results
-        results.write(System.out, FileUtils.langTurtle);
-    }
+			// if there is at least 1 more iteration, then we need to add the new results
+			// to the data model and continue the loop
+			if (iteration < maxIterations - 1) {
+				dataModel.add(newResults);
+			}
+
+			// always increment the iteration counter
+			iteration++;
+		} while (iteration < maxIterations);
+
+		// print results
+		results.write(System.out, FileUtils.langTurtle);
+	}
 }
