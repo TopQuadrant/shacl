fefcfb0752c33ef64a243277bd0414bbf1e5d8c3
Ade Sajim <adesajim@semmtech.nl>
2024-02-14 09:12:57 +0100

    Update jena to version 4.10

--- src/main/java/org/topbraid/shacl/validation/sparql/SPARQLSyntaxChecker.java
+++ src/main/java/org/topbraid/shacl/validation/sparql/SPARQLSyntaxChecker.java
@@ -42,118 +42,130 @@ import org.apache.jena.sparql.syntax.ElementSubQuery;
 import org.apache.jena.sparql.syntax.ElementVisitor;
 import org.apache.jena.sparql.syntax.ElementVisitorBase;
 import org.apache.jena.sparql.syntax.PatternVars;
-import org.apache.jena.sparql.syntax.RecursiveElementVisitor;
 import org.topbraid.shacl.vocabulary.SH;
 
 /**
- * Can be used to check for the violation of any of the syntax rules in Appendix A
- * of the SHACL spec, to prevent certain pre-binding scenarios.
- * 
+ * Can be used to check for the violation of any of the syntax rules in Appendix A of the SHACL
+ * spec, to prevent certain pre-binding scenarios.
+ *
  * @author Holger Knublauch
  */
 public class SPARQLSyntaxChecker {
 
-	/**
-	 * Checks whether a given Query violates any of the syntax rules in Appendix A.
-	 * @param query  the Query to check
-	 * @param preBoundVars  the potentially pre-bound variables
-	 * @return an List of error messages (empty if OK)
-	 */
-	public static List<String> checkQuery(Query query, Set<String> preBoundVars) {
-		List<String> results = new LinkedList<>();
-		ElementVisitor elementVisitor = new RecursiveElementVisitor(new ElementVisitorBase()) {
+    /**
+     * Checks whether a given Query violates any of the syntax rules in Appendix A.
+     *
+     * @param query the Query to check
+     * @param preBoundVars the potentially pre-bound variables
+     * @return an List of error messages (empty if OK)
+     */
+    public static List<String> checkQuery(Query query, Set<String> preBoundVars) {
+        List<String> results = new LinkedList<>();
+        ElementVisitor elementVisitor =
+                new ElementVisitorBase() {
 
-			@Override
-			public void startElement(ElementBind el) {
-				if(preBoundVars.contains(el.getVar().getVarName())) {
-					if(SH.valueVar.getVarName().equals(el.getVar().getVarName()) && el.getExpr().isVariable() && el.getExpr().asVar().equals(SH.thisVar)) {
-						// Ignore clauses injected by engine
-					}
-					else {
-						results.add("Query must not reassign the pre-bound variable " + el.getVar() + " in a BIND clause");
-					}
-				}
-			}
+                    @Override
+                    public void visit(ElementBind el) {
+                        if (preBoundVars.contains(el.getVar().getVarName())) {
+                            if (SH.valueVar.getVarName().equals(el.getVar().getVarName())
+                                    && el.getExpr().isVariable()
+                                    && el.getExpr().asVar().equals(SH.thisVar)) {
+                                // Ignore clauses injected by engine
+                            } else {
+                                results.add(
+                                        "Query must not reassign the pre-bound variable "
+                                                + el.getVar()
+                                                + " in a BIND clause");
+                            }
+                        }
+                    }
 
-			@Override
-			public void startElement(ElementData el) {
-				results.add("Query must not contain VALUES clause");
-			}
+                    @Override
+                    public void visit(ElementData el) {
+                        results.add("Query must not contain VALUES clause");
+                    }
 
-			@Override
-			public void startElement(ElementFilter el) {
-				checkExpression(el.getExpr());
-			}
+                    @Override
+                    public void visit(ElementFilter el) {
+                        checkExpression(el.getExpr());
+                    }
 
-			@Override
-			public void startElement(ElementMinus el) {
-				results.add("Query must not contain MINUS clause");
-			}
+                    @Override
+                    public void visit(ElementMinus el) {
+                        results.add("Query must not contain MINUS clause");
+                    }
 
-			@Override
-			public void startElement(ElementService el) {
-				results.add("Query must not contain SERVICE clause");
-			}
+                    @Override
+                    public void visit(ElementService el) {
+                        results.add("Query must not contain SERVICE clause");
+                    }
 
-			@Override
-			public void startElement(ElementSubQuery el) {
-				if(el.getQuery().isQueryResultStar()) {
-					Set<Var> queryVars = new LinkedHashSet<>() ;
-					PatternVars.vars(queryVars, el.getQuery().getQueryPattern()) ;
-					for(String varName : preBoundVars) {
-						if(!SH.currentShapeVar.getVarName().equals(varName) && !SH.shapesGraphVar.getVarName().equals(varName)) {
-							if(!queryVars.contains(Var.alloc(varName))) {
-								results.add("Sub-query must return all potentially pre-bound variables including $" + varName);
-							}
-						}
-					}
-				}
-				else {
-					VarExprList project = el.getQuery().getProject();
-					for(String varName : preBoundVars) {
-						if(!SH.currentShapeVar.getVarName().equals(varName) && !SH.shapesGraphVar.getVarName().equals(varName)) {
-							if(!project.contains(Var.alloc(varName))) {
-								results.add("Sub-query must return all potentially pre-bound variables including $" + varName);
-							}
-						}
-					}
-				}
-			}
-			
-			private void checkExpression(Expr expr) {
-				final ElementVisitor parent = this;
-				expr.visit(new ExprVisitorFunction() {
-					@Override
-					public void visit(ExprFunctionOp funcOp) {
-						if(funcOp.isGraphPattern()) {
-							funcOp.getElement().visit(parent);
-						}
-					}
-					@Override
-					public void visit(NodeValue nv) {
-					}
-					@Override
-					public void visit(ExprVar nv) {
-					}
-					@Override
-					public void visit(ExprAggregator eAgg) {
-					}
-					@Override
-					public void visit(ExprNone exprNone) {
-					}
-					@Override
-					protected void visitExprFunction(ExprFunction func) {
-						for(Expr expr : func.getArgs()) {
-							expr.visit(this);
-						}
-					}
-					@Override
-					public void visit(ExprTripleTerm tripleTerm) {
-					}
-				});
-			}
-		};
-		query.getQueryPattern().visit(elementVisitor);
-		return results;
-	}
+                    @Override
+                    public void visit(ElementSubQuery el) {
+                        if (el.getQuery().isQueryResultStar()) {
+                            Set<Var> queryVars = new LinkedHashSet<>();
+                            PatternVars.vars(queryVars, el.getQuery().getQueryPattern());
+                            for (String varName : preBoundVars) {
+                                if (!SH.currentShapeVar.getVarName().equals(varName)
+                                        && !SH.shapesGraphVar.getVarName().equals(varName)) {
+                                    if (!queryVars.contains(Var.alloc(varName))) {
+                                        results.add(
+                                                "Sub-query must return all potentially pre-bound variables including $"
+                                                        + varName);
+                                    }
+                                }
+                            }
+                        } else {
+                            VarExprList project = el.getQuery().getProject();
+                            for (String varName : preBoundVars) {
+                                if (!SH.currentShapeVar.getVarName().equals(varName)
+                                        && !SH.shapesGraphVar.getVarName().equals(varName)) {
+                                    if (!project.contains(Var.alloc(varName))) {
+                                        results.add(
+                                                "Sub-query must return all potentially pre-bound variables including $"
+                                                        + varName);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    private void checkExpression(Expr expr) {
+                        final ElementVisitor parent = this;
+                        expr.visit(
+                                new ExprVisitorFunction() {
+                                    @Override
+                                    public void visit(ExprFunctionOp funcOp) {
+                                        if (funcOp.isGraphPattern()) {
+                                            funcOp.getElement().visit(parent);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void visit(NodeValue nv) {}
+
+                                    @Override
+                                    public void visit(ExprVar nv) {}
+
+                                    @Override
+                                    public void visit(ExprAggregator eAgg) {}
+
+                                    @Override
+                                    public void visit(ExprNone exprNone) {}
+
+                                    @Override
+                                    protected void visitExprFunction(ExprFunction func) {
+                                        for (Expr expr : func.getArgs()) {
+                                            expr.visit(this);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void visit(ExprTripleTerm tripleTerm) {}
+                                });
+                    }
+                };
+        query.getQueryPattern().visit(elementVisitor);
+        return results;
+    }
 }
